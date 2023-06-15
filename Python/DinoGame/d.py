c060c620abaded4ff928a7d647d108aa19973a1a
maxZapara <max.e.zapara@gmail.com>
2023-06-15 14:47:33 +0300

    Added Dino Game

---
+++ Python/DinoGame/d.py
@@ -0,0 +1,16 @@
+import pygame
+print (pygame.font.get_fonts())
+#witalc', 'switalt', 'swmap', 'swmath', 'swmeteo', 'swmono', 'swmusic', 'swromnc', 'swromnd',
+#  'swromns', 'swromnt', 'swscrpc', 'swscrps', 'swsimp', 'swtxt', 'swgdt', 'swlink', 'solidw
+# rksgdt', 'inspectionxpertgdtframes', 'inspectionxpertgdtnofrms', 'dsiso1', 'dsiso1bold', 'ds
+# iso1bolditalic', 'dsiso1italic', '3dsbold', '3dsbolditalic', '3dsitalic', '3ds', '3dsfonticoncid', 
+#'courier10pitch', 'dutch801roman', 'dutch801', 'universalmath1', 'monospace821', 'swiss721out
+# line', 'swiss721', 'swiss721condensed', 'catiasymbols', 'symbolmonospaced', 'symbolproportion
+# al', 'bookantiquaполужирный', 'bookantiquaполужирныйкурсив', 'bookantiquaкурсив', 'arialполуж
+# ирный', 'arialполужирныйкурсив', 'arialкурсив', 'bookantiqua', 'bookmanoldstyle', 'bookmanolds
+# tyleполужирный', 'bookmanoldstyleполужирныйкурсив', 'bookmanoldstyleкурсив', 'bookshelfsymbol7', 
+# 'century', 'dubai', 'dubaimedium', 'dubairegular', 'garamond', 'garamondполужирный', 'garamondку
+# сив', 'centurygothicполужирный', 'centurygothicполужирныйкурсив', 'centurygothicкурсив', 'leelawadee', 
+#  'leelawadeeполужирный', 'microsoftuighurполужирный', 'microsoftuighur', 'monotypecorsiva', 'extra', 
+# 'msreferencesansserif', 'msreferencespecialty', 'wingdings2', 'wingdings3', 'cyrillicgoth', 
+# 'lucidacalligraphy', 'breeze', 'gosttypeau', 'gosttypea']
